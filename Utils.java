7ae36a227d27b0467d1a091ccadc3cb4b1cc2b3d
Ahmed SAMI MOHAMED <asyn.nile@gmail.com>
2017-12-10 21:57:55 +0100

    fix issue with parsing date

--- Utils.java
+++ Utils.java
@@ -4,8 +4,12 @@ public final class Utils {
     /**
      * get the name from scenario tag
      */
-    public static String retrieveName(String scenario)     {
+    public static String retrieveName(String scenario) {
         String name = scenario.replace("<SCENARIO ", "");
         return name.replace(">", "");
+    }
+
+    public static String fixDate(String date) {
+        return date.replace("_", "-1");
     }
 }
