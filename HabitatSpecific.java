eda3681b11dad357fffb0a41a2c267cd0f154b6e
Ahmed SAMI MOHAMED <asyn.nile@gmail.com>
2017-12-14 00:28:39 +0100

    Generating Habitat

---
+++ HabitatSpecific.java
@@ -0,0 +1,16 @@
+System.out.println("Execution du scenario " + this.l_scenarios.get(num).getNomScenario() + "... ");
+switch(num) {
+	case 0: // Scenario bonjour
+	break;
+	case 1: // Scenario soiree
+	break;
+	case 2: // Scenario soiree_musique
+	break;
+	case 3: // Scenario depart
+	break;
+	case 4: // Scenario noel1
+	break;
+	case 5: // Scenario noel2
+	break;
+	case 6: // Scenario noel3
+	break;
